a208617dfa3250f17c6fb009b9c8f38cc9307414
José Luis <hansokaz@gmail.com>
2021-06-26 20:11:22 +0200

    Added draw render of toprated

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,14 +1,77 @@
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-const Home = () => {
+const Home = (props) => {
+
+    // HOOKS
+    const [movieData, setMovieData] = useState([])
+
+    // HANDLERS
+    const updateMovieData = (e) => {
+        setMovieData({...movieData, [e.target.name]: e.target.value})
+    }
+
+    // REACT STATES
+
+    useEffect(()=>{
+        topRatedMovies();
+    },[]);
+
+    useEffect(()=>{
+
+    });
+
+    const topRatedMovies = async () => {
+        try{
+            console.log("hoka")
+            let res = await axios.get(`http://localhost:3006/movies` );
+            console.log(res.data.results)
+            setMovieData(res.data.results);
+
+            // console.log(movieData.movie)
+
+        }catch{
+            console.log("error loading")
+        }
+    
+    }
+
+    const baseImgUrl = "https://image.tmdb.org/t/p"
+    const size = "w500" 
+
     return (
         <div className="vistaHome">
             <div className="tarjeta">
-                soy una tarjeta dentro de Home
+            {movieData.map((movie, index) => (
+           
+            <div key={index} className="appointmentCard1">
+
+            <img src={`${baseImgUrl}/${size}${movie.poster_path}`}  alt="poster"/>
+
+            <p> Movie: {movie.title} </p>
+            <p> Rated : {movie.vote_average} </p>
+            <p> Id : {movie.id} </p>
+            // Crear en CSS gradientes con transparencias, overflow: scroll.
+             {/* <p> DENTIST : {movie.genre_ids} </p> */}
+
+             
+             {/* <div className="buttons1">
+               <div
+                 className="buttonUpdateA"
+                 onClick={() => saveAppointment(appointment)}
+               >
+                 UPDATE
+               </div>
+               <div className="buttonDeleteA" onClick={() => deleteAppointment(appointment)}>REMOVE</div>
+             </div> */} 
+            </div>
+            ))}
+
             </div>
         </div>
     )
 }
 
-export default Home;
+export default connect()(Home);
