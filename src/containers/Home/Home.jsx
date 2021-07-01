d083cce55ca89e729c21114751c65c2813a4dd70
José Luis <hansokaz@gmail.com>
2021-07-01 17:40:18 +0200

    working on search by title

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -6,7 +6,9 @@ import axios from 'axios';
 const Home = (props) => {
 
     // HOOKS
-    const [movieData, setMovieData] = useState([])
+    const [movieData, setMovieData] = useState([
+        // topRated: ''
+    ])
 
     // HANDLERS
     const updateMovieData = (e) => {
@@ -27,45 +29,59 @@ const Home = (props) => {
         try{
             // let res = await axios.get(`http://localhost:3006/movies` );
             let res = await axios.get(`https://api.themoviedb.org/3/movie/300/recommendations?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1` );
+            // setMovieData({...movieData, topRated: res.data.results});
             setMovieData(res.data.results);
+            console.log(res.data.results)
         }catch{
             console.log("error loading")
         }
     
     }
 
+    const searchMovie = async () => {
+        try {
+            let body = {
+                title: movieData.movie
+            }
+            let res = await axios.post(`http://localhost:3006/title`, body);
+            setMovieData(res.data.results);
+        }catch {
+
+        }
+    }
+
     const baseImgUrl = "https://image.tmdb.org/t/p"
-    const size = "w500" 
+    const size = "w200" 
 
     return (
         <div className="vistaHome">
-            <div className="tarjeta">
-            {movieData.map((movie, index) => (
-           
-            <div key={index} className="appointmentCard1">
 
-            <img src={`${baseImgUrl}/${size}${movie.poster_path}`}  alt="poster"/>
+            <input className="searchMovie" name="movie" placeholder="Movie name" onBlur={updateMovieData}></input>
+            <button onClick={searchMovie}>Search</button>
+            <p>TOP RATED MOVIES</p>
+            <div className="contentMovies">
+                {movieData.map((movie, index) => (
+                <div className="movieCard">
+                    <div key={index} className="movieImg">
 
-            <p> Movie: {movie.title} </p>
-            <p> Rated : {movie.vote_average} </p>
-            <p> Id : {movie.id} </p>
-            // Crear en CSS gradientes con transparencias, overflow: scroll.
-             {/* <p> DENTIST : {movie.genre_ids} </p> */}
-
-             
-             {/* <div className="buttons1">
-               <div
-                 className="buttonUpdateA"
-                 onClick={() => saveAppointment(appointment)}
-               >
-                 UPDATE
-               </div>
-               <div className="buttonDeleteA" onClick={() => deleteAppointment(appointment)}>REMOVE</div>
-             </div> */} 
+                        <img src={`${baseImgUrl}/${size}${movie.poster_path}`}  alt="poster"/>
+                        <div className="movieData">
+                            <p> Movie: {movie.title} </p>
+                            <p> Rated : {movie.vote_average} </p>
+                            {/* <p> Id : {movie.id} </p> */}
+                        </div>
+                        {/* Crear en CSS gradientes con transparencias, overflow: scroll. */}
+                         {/* <p> DENTIST : {movie.genre_ids} </p> */}           
+                         {/* <div className="buttons1">
+                           <div className="buttonUpdateA" onClick={() => saveAppointment(appointment)}>UPDATE</div>
+                           <div className="buttonDeleteA" onClick={() => deleteAppointment(appointment)}>REMOVE</div>
+                         </div> */} 
+                    </div>
+                </div>
+                ))}
             </div>
-            ))}
 
-            </div>
+            
         </div>
     )
 }
